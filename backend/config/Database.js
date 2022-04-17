7863573727f06b32613c959ac1e0c39ec9b9de99
unknown <fajarmh2@gmail.com>
2022-04-17 16:48:48 +0700

    Connecting database

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_ROOT, process.env.DB_PASSWORD, {
+const db = new Sequelize(process.env.DB_NAME, 'root', process.env.DB_PASSWORD, {
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+    "dialect": "mysql"
 });
 
 export default db;
--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,18 @@ import dotenv from 'dotenv';
 import express from 'express';
 import router from './routes/index.js';
 import cookieParser from 'cookie-parser';
+import db from './config/Database.js';
 
 dotenv.config();
 const app = express();
 const PORT = 5000;
+
+try {
+    await db.authenticate();
+    console.log("Database connected");
+} catch(err) {
+    console.log(err);
+}
 
 app.use(router);
 app.use(express.json());
