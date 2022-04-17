5476b1f08cc7575d9c7881a3e551101298f8d32d
unknown <fajarmh2@gmail.com>
2022-04-17 17:32:18 +0700

    Create get user controller and success test in postman

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,5 +1,16 @@
+import { Users } from "../models/UserModel.js";
+
 export class User {
-    async getUsers() {
-        
+    async getUsers(req, res) {
+        try {
+            const users = await Users.findAll({
+                attributes: ['id', 'name', 'email']
+            })
+
+            res.json(users);
+        } catch(err) {
+            console.log(err);
+            res.status(500).json({msg: "Internal server error"});
+        }
     }
 }
